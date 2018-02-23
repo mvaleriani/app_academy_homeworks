714c765a09f559a407a354c3cbb71a2d7dec48c4
mvaleriani <michaelvaleriani@gmail.com>
2018-02-22 23:39:34 -0800

    debug LRU_Cache

--- w2d5/LRU_Cache.rb
+++ w2d5/LRU_Cache.rb
@@ -1,7 +1,7 @@
 class LRUCache
     attr_accessor :c_hash, :c_arr
 
-    def initialize
+    def initialize(max_size)
       @c_hash = {}
       @c_arr = []
     end
@@ -14,14 +14,17 @@ class LRUCache
     def add(el)
       el_hash = el.hash
       if self.c_hash[el_hash].nil?
+        if count == max_size - 1
+          old = self.c_arr.shift
+          self.c_hash[old.hash] = nil
+        end
         self.c_arr << el
         self.c_hash[el_hash] = count - 1
-
-
       else
-        self.c_hash[el_hash] = el
+        self.c_hash[el_hash] = count - 1
+        self.c_arr.delete(el)
+        self.c_arr << el
       end
-
       # adds element to cache according to LRU principle
 
     end
