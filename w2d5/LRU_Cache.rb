6e23f9ed804f562b9f5e030e4e100c26ceb8b467
mvaleriani <michaelvaleriani@gmail.com>
2018-02-22 23:31:45 -0800

    implement LRU_Cache

--- w2d5/LRU_Cache.rb
+++ w2d5/LRU_Cache.rb
@@ -1,4 +1,6 @@
 class LRUCache
+    attr_accessor :c_hash, :c_arr
+
     def initialize
       @c_hash = {}
       @c_arr = []
@@ -10,17 +12,26 @@ class LRUCache
     end
 
     def add(el)
-      # self.c_
+      el_hash = el.hash
+      if self.c_hash[el_hash].nil?
+        self.c_arr << el
+        self.c_hash[el_hash] = count - 1
+
+
+      else
+        self.c_hash[el_hash] = el
+      end
+
       # adds element to cache according to LRU principle
 
     end
 
     def show
+      p self.c_arr
       # shows the items in the cache, with the LRU item first
     end
 
     private
-    attr_accessor :c_hash, :c_arr
     # helper methods go here!
 
   end
