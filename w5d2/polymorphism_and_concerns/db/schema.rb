7f2eb7599069a442d4e78b5ffdf8a0a95a50dc6b
mvaleriani <michaelvaleriani@gmail.com>
2018-03-13 08:41:26 -0700

    add polymorphism

--- w5d2/polymorphism_and_concerns/db/schema.rb
+++ w5d2/polymorphism_and_concerns/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714053409) do
+ActiveRecord::Schema.define(version: 20180313153711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20170714053409) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "toys", force: :cascade do |t|
+    t.string "name"
+    t.bigint "toyable_id"
+    t.string "toyable_type"
+    t.index ["toyable_type", "toyable_id"], name: "index_toys_on_toyable_type_and_toyable_id"
   end
 
 end
